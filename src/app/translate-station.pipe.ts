cd522d4ba0436655ab7de0a1bc6be45dd98d16ef
SvenDepickere <sven.depickere@student.howest.be>
2019-12-09 16:55:36 +0100

    Create translate-station.pipe.ts

---
+++ src/app/translate-station.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import {Station} from "./station";
+
+@Pipe({
+  name: 'translateStation',
+  pure: false
+})
+export class TranslateStationPipe implements PipeTransform {
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  transform(station: Station): string {
+    if (this.translateService.currentLang !== 'zh-TW'){
+      return `${station.eng站名}  (${station.traWebsiteCode})`
+    } else {
+      return `${station.站名}  (${station.traWebsiteCode})`
+    }
+  }
+
+}
