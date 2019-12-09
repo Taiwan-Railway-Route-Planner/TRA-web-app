1b867464a5886b9989f6ddb9289a9587171c0ac5
SvenDepickere <sven.depickere@student.howest.be>
2019-12-09 16:37:55 +0100

    create station class

---
+++ src/app/station.ts
@@ -0,0 +1,14 @@
+export class Station {
+  '時刻表編號': number;
+  "traWebsiteCode": string;
+  '站名': string;
+  'eng站名': string;
+  'routeCode': number[];
+  'gradeStation': {
+    'status': boolean;
+    'value': number;
+  };
+  'stops': number;
+  '縣市': string;
+  'eng縣市': string;
+}
