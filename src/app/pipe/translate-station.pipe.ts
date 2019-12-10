110a087e066dcae4792f87948d7555710163a4cd
SvenDepickere <sven.depickere@student.howest.be>
2019-12-10 09:31:02 +0100

    reorder in separated folders

--- src/app/pipe/translate-station.pipe.ts
+++ src/app/pipe/translate-station.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import {Station} from "./station";
+import {Station} from "../station";
 
 @Pipe({
   name: 'translateStation',
