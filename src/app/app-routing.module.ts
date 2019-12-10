965d2329e54141c9d5366aa2274dd31c3d2cbb08
SvenDepickere <sven.depickere@student.howest.be>
2019-12-10 15:46:33 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { TrainOverviewComponent } from "./train-overview/train-overview.component";
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
+  { path: '/train', component: TrainOverviewComponent },
 ];
 
 @NgModule({
