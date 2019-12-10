1011c2740b1e0a244a9e7ea7535aa9f1b39a3da9
SvenDepickere <sven.depickere@student.howest.be>
2019-12-10 08:34:35 +0100

    clean up main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -12,74 +12,27 @@ import { filter, map, takeUntil, tap } from "rxjs/operators";
 })
 export class MainPageComponent implements OnInit {
 
-  stationList: any;
+  stationInfo: any;
+  stationList: Station[];
+  departureStation: Station;
+  arrivalStation: Station;
 
-
-  protected stationInfo: Station[];
-
-  public stationCtrl: FormControl = new FormControl();
-
-  public stationFilterCtrl: FormControl = new FormControl();
-
-  public searching: boolean = false;
-
-  public filteredStations: ReplaySubject<Station[]> = new ReplaySubject<Station[]>(1);
-
-  protected _onDestroy = new Subject<void>();
-
-  constructor(
-    private requestService: RequestService,
-  ) { }
+  constructor(private requestService: RequestService,) { }
 
   ngOnInit() {
-
     this.getStationList();
+  }
 
-    this.stationFilterCtrl.valueChanges
-      .pipe(
-        filter(search => !!search),
-        tap(() => this.searching = true),
-        takeUntil(this._onDestroy),
-        map(search => {
-          if (!this.stationInfo) {
-            return [];
-          }
-
-          return this.stationInfo.filter(station =>
-            station['eng站名'].toLowerCase().indexOf(search) > -1
-            || station['站名'].toLowerCase().indexOf(search) > -1
-            || station['traWebsiteCode'].toLowerCase().indexOf(search) > -1
-            // || station['時刻表編號'].toString().toLowerCase().indexOf(search) > -1
-          );
-
-        })
-      )
-      .subscribe(filterStations => {
-          this.searching = false;
-          this.filteredStations.next(filterStations);
-        },
-        error => {
-          // no errors in our simulated example
-          this.searching = false;
-          // handle error...
-        });
-
+  openStationList(departureOrArrivalStation: boolean){
 
   }
 
   getStationList() : void {
     let _self = this;
     this.requestService.getStation().subscribe(function (stationListData) {
-      _self.stationList = stationListData;
-      _self.stationInfo = stationListData.stations;
-      _self.filteredStations.next(stationListData.stations);
-      _self.searching = false;
+      _self.stationInfo = stationListData;
+      _self.stationList = stationListData.stations;
     });
-  }
-
-  ngOnDestroy() {
-    this._onDestroy.next();
-    this._onDestroy.complete();
   }
 
 }
