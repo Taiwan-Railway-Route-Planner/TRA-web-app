effb9d46d9b1a7f5153dcebda46275d64468f557
SvenDepickere <sven.depickere@student.howest.be>
2019-12-10 08:34:42 +0100

    init

---
+++ src/app/main-page/station-search/station-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-station-search',
+  templateUrl: './station-search.component.html',
+  styleUrls: ['./station-search.component.sass']
+})
+export class StationSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
