f8e868125e79aedd89bb20226ddd197bed8c2698
SvenDepickere <sven.depickere@student.howest.be>
2019-12-10 10:16:12 +0100

    Update station-info.service.ts

--- src/app/service/station-info.service.ts
+++ src/app/service/station-info.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Station } from "../station";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StationInfoService {
 
+  private stationInfo: any;
+  private _pureStationList: Station[];
+  private _filteredStationList: Station[];
+
   constructor() { }
+
+  public initService(stationInfo: any){
+    this.stationInfo = stationInfo;
+    this._pureStationList = stationInfo.stations;
+  }
+
+  getCounties(): any {
+    return this.stationInfo.counties;
+  }
+
+  getFilterStation(): Station[] {
+    return this._filteredStationList;
+  }
+
+  updateFilterStation(stationToFilter: Station): void {
+    this._filteredStationList = this._pureStationList.filter(station => station.時刻表編號 === stationToFilter.時刻表編號);
+  }
+
+
 }
