e52cf3006ac56586cdcec99f50f3ca32a69dc1cb
SvenDepickere <sven.depickere@student.howest.be>
2019-12-11 16:58:56 +0100

    Update station-search.component.ts

--- src/app/main-page/station-search/station-search.component.ts
+++ src/app/main-page/station-search/station-search.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Station } from "../../class/station";
-import { StateStationService } from "../../service/stateStation.service";
 import { TranslateService } from "@ngx-translate/core";
 import { map, startWith } from "rxjs/operators";
 
@@ -11,44 +10,47 @@ import { map, startWith } from "rxjs/operators";
 })
 export class StationSearchComponent implements OnInit {
 
-  @Input() departureOrArrivalStation: boolean;
+  @Input() placeholderLabel: string;
+  @Input() stationInfo: any;
+  @Output() stationSelect = new EventEmitter<Station>();
   @Output() stopEvent = new EventEmitter();
 
   selectedStation: Station;
-  prop$;
-  stationInfo$;
+  propStation$;
+  propCounties$;
 
 
   constructor(
-    private state: StateStationService,
-    private translateService: TranslateService,
+    private translateService: TranslateService
   ) { }
 
   ngOnInit() {
-    this.stationInfo$ = this.state.stationInfo;
 
-    this.prop$ = this.translateService.onLangChange.pipe(
+    this.propCounties$ = this.translateService.onLangChange.pipe(
       startWith({lang: this.translateService.currentLang}),
       map(langChangeEvent => {
         if (langChangeEvent.lang !== 'zh-TW'){
-          // return ['eng縣市', 'eng站名']
           return 'eng縣市'
         } else {
-          // return ['縣市', '站名']
           return '縣市'
+        }
+      }),
+    );
+
+    this.propStation$ = this.translateService.onLangChange.pipe(
+      startWith({lang: this.translateService.currentLang}),
+      map(langChangeEvent => {
+        if (langChangeEvent.lang !== 'zh-TW'){
+          return 'eng站名'
+        } else {
+          return '站名'
         }
       }),
     );
   }
 
   saveValues() {
-    if (this.departureOrArrivalStation){
-      this.state.updateDepartureStation(this.selectedStation)
-    } else {
-      this.state.updateArrivalStation(this.selectedStation);
-    }
-    this.selectedStation = undefined;
-    this.departureOrArrivalStation = !this.departureOrArrivalStation;
+    this.stationSelect.emit(this.selectedStation);
   }
 
   selectThisStation(selectedStation: Station){
