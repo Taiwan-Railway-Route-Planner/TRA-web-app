b2b6472feb994e8b4c39b4336bb43338e9d6e4e9
DepickereSven <sven.depickere@student.howest.be>
2019-12-20 09:39:27 +0100

    init sandbox

---
+++ src/modules/app/app.sandbox.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { RequestService, StateStationService } from './services';
+import { Observable } from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {Station} from './types';
+
+@Injectable()
+export class AppSandbox {
+
+  stationList = this.state.stationList;
+  countyList = this.state.countyList;
+  departureStation = this.state.departureStation;
+  arrivalStation = this.state.arrivalStation;
+  timeDetails = this.state.timeDetails;
+  travelDetails = this.state.travelDetails;
+
+  constructor(
+    private request: RequestService,
+    private state: StateStationService
+  ) {}
+
+  public loadInfoData(): Observable<any[]> {
+    return this.request.getInfoList().pipe(
+      tap(x => {
+        this.state.updateStationList(x.stations);
+        this.state.updateCountyList(x.counties);
+      })
+    );
+  }
+
+  public getARoute(travel: any): Observable<any> {
+    const empty = {};
+    const time = { ...empty , time: travel.time.time, date:  travel.time.date.real};
+    this.state.updateTimeDetails(time);
+    return this.request.getTheRoute(JSON.stringify(travel)).pipe(
+      tap(x => this.state.updateTravelDetails(x))
+    );
+  }
+
+  public updateDepartureStation(station: Station): void {
+    this.state.updateDepartureStation(station);
+  }
+
+  public updateArrivalStation(station: Station): void {
+    this.state.updateArrivalStation(station);
+  }
+
+}
